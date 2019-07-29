e3186a6c8cedada7febc5e7f2364748d74166beb
carlosdionell <37004879+carlosdionell@users.noreply.github.com>
2019-07-29 15:06:49 -0500

    tarea de torre de hanei

---
+++ torre de hanei.py
@@ -0,0 +1,18 @@
+
+
+#origen, auxiliar, destino
+
+def imprime_movimiento(origen, destino):
+    print("mueve desde " + str(origen) + " a " + str(destino))
+
+# origen: front, destino, auxiliar
+def torres(n, origen, destino, auxiliar):
+    if n == 1:
+        imprime_movimiento(origen, destino)
+    else:
+        torres(n-1, origen, auxiliar, destino)
+        torres(1, origen, destino, auxiliar)
+        torres(n-1, auxiliar, destino, origen)
+
+
+torres(5, "o", "d", "a");
